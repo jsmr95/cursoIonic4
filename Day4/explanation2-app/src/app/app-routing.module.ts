0f28366621de4f844c93c741c4357022492805ec
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-04-25 18:59:25 +0200

    Añado avances sobre detalles de la lista

--- Day4/explanation2-app/src/app/app-routing.module.ts
+++ Day4/explanation2-app/src/app/app-routing.module.ts
@@ -9,12 +9,13 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
+    loadChildren: './pages/home/home.module#HomePageModule'
   },
   {
     path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  }
+    loadChildren: './pages/list/list.module#ListPageModule'
+  },
+  { path: 'details/:index', loadChildren: './pages/details/details.module#DetailsPageModule' }
 ];
 
 @NgModule({
