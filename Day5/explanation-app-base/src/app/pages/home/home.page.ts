3475188efdc5a19767e646b54d0877f5754ea4e4
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-04-29 16:43:27 +0200

    Added Alerts

--- Day5/explanation-app-base/src/app/pages/home/home.page.ts
+++ Day5/explanation-app-base/src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {
+  constructor(private alertController: AlertController) {
 
   }
+
+  //Para mandar alertas
+  public async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [{
+        text: 'Cancel',
+        handler: () => {
+          console.log('Cancel pressed!');
+        }
+      },{
+        text: 'OK',
+        handler: () => {
+          console.log('Ok pressed!');
+        }
+      }
+    ]
+    });
+
+    await alert.present();
+  }
+
 }
