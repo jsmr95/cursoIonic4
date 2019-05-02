972ede7fb5696e5bbd80de2e1af50cdce5004977
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-05-02 09:33:13 +0200

    Comento la función

--- Day6/explanationPipe-app/src/app/services/foro.service.ts
+++ Day6/explanationPipe-app/src/app/services/foro.service.ts
@@ -31,14 +31,17 @@ export class ForoService {
 
 
  public getPostsWithOutBrands() {
-
+   //Obtengo los posts
    return this.getPostsFromServer()
      .pipe(flatMap((posts) => {
+       //uno las marcas con los posts
      return this.getBrandsFromServer().pipe(zip(of(posts)));
    }), map((brandsAndPosts) => {
      const [brands, posts] = brandsAndPosts;
      posts.map((post) => {
+       //en cada mensaje, checkeo si tiene alguna de sus marcas en el mensaje
        for(let i = 0; i < brands.length; i++){
+         //si lo incluye, realizo los cambios
          if(post.mensaje.includes(brands[i].name) && !brands[i].isSoldBuyUs){
            let len = brands[i].name.length;
            let sustituto = '';
