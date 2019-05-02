41f2362b6fe1c0f9b8c292aeb27f20f054d45577
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-05-02 09:29:32 +0200

    Added exercise 3 solved

--- Day6/explanationPipe-app/src/app/home/home.page.ts
+++ Day6/explanationPipe-app/src/app/home/home.page.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
+import { ForoService } from '../services/foro.service';
 import { TruncatePipe } from '../pipes/truncate.pipe';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  public mensajes = [];
 
   constructor(private toastController: ToastController,
               private TruncatePipe: TruncatePipe,
-              private translate: TranslateService) {
+              private translate: TranslateService,
+            private foroService: ForoService) {
 
+  }
+
+  ngOnInit(){
+    this.getMensajes();
   }
   //Podemo hacer INJECTABLE a nuestro pipe para meterlo en los metodos de un controlador
   public async presentToast(): Promise<void>{
@@ -31,5 +40,15 @@ export class HomePage {
     });
     toast.present();
   }
+
+  public getMensajes(){
+    this.foroService.getPostsWithOutBrands().subscribe((posts) => {
+      for(let i = 0; i <posts.length; i++){
+        this.mensajes.push(posts[i].mensaje);
+      }
+      return this.mensajes;
+    });
+}
+
 
 }
--- Day6/explanationPipe-app/src/app/services/foro.service.ts
+++ Day6/explanationPipe-app/src/app/services/foro.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+let data = require('./data.json');
+
+let { of } = require('rxjs');
+let { map, flatMap, zip } = require('rxjs/operators');
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForoService {
 
-  constructor() { }
+  constructor() {
+    console.log(data)
+
+
+
+
+
+
+ }
+
+ public getBrandsFromServer() {
+   return of(data.brands);
+ };
+
+ public getPostsFromServer() {
+   return of(data.posts);
+ };
+
+
+
+
+ public getPostsWithOutBrands() {
+
+   return this.getPostsFromServer()
+     .pipe(flatMap((posts) => {
+     return this.getBrandsFromServer().pipe(zip(of(posts)));
+   }), map((brandsAndPosts) => {
+     const [brands, posts] = brandsAndPosts;
+     posts.map((post) => {
+       for(let i = 0; i < brands.length; i++){
+         if(post.mensaje.includes(brands[i].name) && !brands[i].isSoldBuyUs){
+           let len = brands[i].name.length;
+           let sustituto = '';
+           for(let j = 0; j < len; j++){
+             sustituto += '*';
+           }
+           post.mensaje = post.mensaje.replace(brands[i].name, sustituto);
+         }
+       }
+     });
+     return posts;
+   }));
+ };
+
+
 }
